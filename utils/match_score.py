c1a403e0769b656cf4e6e7ba3111c18ecf8efd7e
Hafiz Muhammad Rayyan Asif <asifrayyan30@gmail.com>
2026-07-24 10:08:44 -0700

    Update model version in match_score.py

--- utils/match_score.py
+++ utils/match_score.py
@@ -37,7 +37,7 @@ def match_score(summery, job_description):
 
     llm = ChatGroq(
         api_key=os.getenv("LLM_API_KEY"),
-        model="openai/gpt-oss-safeguard-20b"
+        model="openai/gpt-oss-120b"
     )
 
     System_prompt = System_prompt.partial(
